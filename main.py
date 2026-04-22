a099c7b863c2922ec5699a185fad3e3ee8ae1574
23aaustin-birds <23aaustin@wakatipu.school.nz>
2026-04-22 13:51:20 +1200

    Continued on the ordering_infromation function and managed to successfully print infromation.

--- main.py
+++ main.py
@@ -8,30 +8,48 @@ def user_information(user_dictionary):
 # Collects order
 def ordering_information(user_dictionary):
     menu_items = {
-        "Dalh with rice" : ["M", "VE", "V", "GF", "DF"],
+        "Dalh with rice" : ["M", "V", "VE", "GF", "DF"],
         "Risoto" : ["M", "V", "GF"],
-        "Tagine" : ["M", "VE", "V", "GF", "DF"],
+        "Tagine" : ["M", "V", "VE", "GF", "DF"],
         "Lasagna" : ["M", "V"]
     }
-    for item in menu_items:
-        print(item)
-        for option in menu_items[item]:
-            print(f"   {option}")
 
-    input("What menu options would you like? ")
+    while True:
+        print("Food items avalibles:")
+        
+        number = 1
+        for item in menu_items: # Extracts food items from meun_items (eg, "Dalh with Rice", "Risoto" etc.)
+            options = ""
+            for option in menu_items[item]: # Extracts the next option in the values lists  (eg, "V", "VE", etc.)
+                options =  options + ", " + option # Added all previously extracted options, a ", " and the new options
+            
+            print(f"    {number}. {item} ({options[2:]})") # String splice removes extra ", " for the start of the options string
+            number += 1
+        
+        print(f"Or:\n   {number}. Save & Continue")
+        input("> ")
+
+
+            
+
+
+
+        break
 
 # Devlivery/pick up information
 def delivery_infromation(user_dictionary):
     print("Would you like to:" \
-    "a. Pick Up" \
-    "b. Delivered")
+    "1. Pick Up" \
+    "2. Delivered")
     user_answer = input("> ")
 
-    if user_answer == "a":
+    if user_answer == "1":
         user_dictionary["Delivery"] = False
-    else:
+    if user_answer == "2":
         user_dictionary["Delivery"] = True
         user_dictionary["Adress"] = input("Delivery Address: ")
+    else:
+        print("Please enter either '1' or '2'.")
     
     return user_dictionary
 
@@ -85,7 +103,7 @@ def main_menu():
         temp_dic = user_information(temp_dic)
 
         print("2. Pick your food")
-        # ordering_information(temp_dic)
+        ordering_information(temp_dic)
 
         print("3. Delivery/Pick Up Information")
         # temp_dic = delivery_infromation(temp_dic)
@@ -93,6 +111,8 @@ def main_menu():
         print("4. Confirm Order")
         print_recipt(temp_dic)
         if order_confirmed():
+            number = 1 # Generate random number
+            all_orders[number] = temp_dic
             print("Thanks for coming to Mountain Meals! We hope you enjoy your food.")
         else:
             print("Your order has been cancalled! We hope you come to Mountain Meals in the future.")
